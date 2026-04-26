f60272da97c98d9a3a946d5e33da73fe77ef3a56
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-26 00:09:37 -0500

    Agrega función para guardar objeto Usuario en archivo
    Implementa el método guardarUsuario que serializa un objeto Usuario y lo guarda en un archivo. Maneja excepciones para asegurar que se notifique al usuario en caso de error durante el proceso de guardado.

--- src/Concepto/Main.java
+++ src/Concepto/Main.java
@@ -1,4 +1,4 @@
-﻿package Concepto;
+package Concepto;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,14 +12,22 @@ public class Main {
         user.contraseña = "1234";
 
         //user.saludar();
+        guardarUsuario(user);
 
-        FileOutputStream fileOut = new FileOutputStream("UsuarioInfo.ser"); // archivo
-        ObjectOutputStream out = new ObjectOutputStream(fileOut); // permite escribir objetos completos en el archivo
-        out.writeObject(user); // escribe el objeto user en el archivo
-        out.close(); // cierra el stream de objetos
-        fileOut.close(); // cierra el stream de archivo // ahorrar recursos
+    }
 
-        System.out.println("El Objeto Ha Sido Guardado");
+    static void guardarUsuario(Usuario user){
+        try {
 
+            FileOutputStream fileOut = new FileOutputStream("UsuarioInfo.ser"); // archivo
+            ObjectOutputStream out = new ObjectOutputStream(fileOut); // permite escribir objetos completos en el archivo
+            out.writeObject(user); // escribe el objeto user en el archivo
+            out.close(); // cierra el stream de objetos
+            fileOut.close(); // cierra el stream de archivo // ahorrar recursos
+
+        }catch (IOException e){
+            System.out.println("Error Al Guardar El Objeto: " + e.getMessage());
+        }
+        System.out.println("El Objeto Ha Sido Guardado Con Exito");
     }
 }

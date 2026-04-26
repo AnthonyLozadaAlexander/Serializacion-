be6e1f237c6162b6cca43a80da34d407c0452a30
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-26 13:58:27 -0500

    Cambia la clase Usuario a final para evitar herencia
    Se modifica la clase Usuario para que sea final, lo que previene que otras clases la extiendan. Esto mejora la seguridad y la integridad de la clase, asegurando que su comportamiento no sea alterado por subclases.

--- src/Concepto/Modelo/Usuario.java
+++ src/Concepto/Modelo/Usuario.java
@@ -2,10 +2,10 @@ package Concepto.Modelo;
 
 import java.io.Serializable;
 
-public class Usuario implements Serializable {
+final class Usuario implements Serializable {
     public String nombre;
     public String contraseña;
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; // Esto es para asegurar que la clase sea compatible con la version de serializacion, no es obligatorio pero recomendado
 
     public void saludar(){
         System.out.println("Hola, mi nombre es " + nombre);

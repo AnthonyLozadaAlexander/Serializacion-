12203c07bc1a6f5718b5613e9a3a50af0f80ab2b
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-25 23:50:57 -0500

    Agrega clase Usuario con atributos y método de saludo

---
+++ src/Concepto/Usuario.java
@@ -0,0 +1,11 @@
+﻿package Concepto;
+
+public class Usuario {
+
+    String nombre;
+    String contraseña;
+
+    public void saludar(){
+        System.out.println("Hola, mi nombre es " + nombre);
+    }
+}

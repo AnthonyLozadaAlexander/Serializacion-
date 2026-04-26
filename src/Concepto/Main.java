f024ebde1367e0eb83f733b8ff4cc9d6cc626a9f
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-26 00:04:25 -0500

    Agrega clase Main para serializar objetos Usuario

---
+++ src/Concepto/Main.java
@@ -0,0 +1,25 @@
+﻿package Concepto;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class Main {
+    static void main(String[] args) throws IOException {
+
+        Usuario user = new Usuario();
+        user.nombre = "Pepito";
+        user.contraseña = "1234";
+
+        //user.saludar();
+
+        FileOutputStream fileOut = new FileOutputStream("UsuarioInfo.ser"); // archivo
+        ObjectOutputStream out = new ObjectOutputStream(fileOut); // permite escribir objetos completos en el archivo
+        out.writeObject(user); // escribe el objeto user en el archivo
+        out.close(); // cierra el stream de objetos
+        fileOut.close(); // cierra el stream de archivo // ahorrar recursos
+
+        System.out.println("El Objeto Ha Sido Guardado");
+
+    }
+}
